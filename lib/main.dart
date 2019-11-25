cddfb8391b4d9e9ddb4881a1277810ecb7afd755
Kael <khang.tran@senecacollege.ca>
2019-11-25 16:22:46 -0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:crwn_social_network/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterShare',
+      debugShowCheckedModeBanner: false,
+      home: Home()
+    );
+  }
+}
